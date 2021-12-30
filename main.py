c8f4472008da8943aad61d9b1ce480caae6db3d4
Alex <alexchizhov90@gmail.com>
2021-12-30 21:00:01 +0300

    Added parameter for request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
+response = requests.get("https://playground.learnqa.ru/api/hello?name=Alex")
 print(response.text) # {"answer":"Hello, someone"}
