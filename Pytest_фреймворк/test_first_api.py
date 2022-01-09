207e259b87f10bde54952379336930496e352e1f
Alex <alexchizhov90@gmail.com>
2022-01-09 05:18:19 +0300

    Added parametrization for first example API test

--- Pytest_фреймворк/test_first_api.py
+++ Pytest_фреймворк/test_first_api.py
@@ -1,10 +1,13 @@
+import pytest
 import requests
 
 class TestFirstAPI:
 
-    def test_hello_call(self):
+    names = ["Vitalii", "Arseniy", ""]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Vitalii"
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -13,6 +16,9 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no 'answer' fueld in response"
 
-        expectrd_response_text = f"Hello, {name}"
+        if name == "":
+            expectrd_response_text = "Hello, someone"
+        else:
+            expectrd_response_text = f"Hello, {name}"
         acttual_response_text = response_dict['answer']
         assert expectrd_response_text == acttual_response_text, "Acttual response text is not correct"
