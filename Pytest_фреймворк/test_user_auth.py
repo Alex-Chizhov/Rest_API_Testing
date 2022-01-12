fc8e7bb3ec170715799e3e95b5b1233401dd194d
Alex <alexchizhov90@gmail.com>
2022-01-12 08:51:49 +0300

    Added test_negative_auth_user

--- Pytest_фреймворк/test_user_auth.py
+++ Pytest_фреймворк/test_user_auth.py
@@ -26,3 +26,32 @@ class TestUserAuth:
         user_id_from_auth_method = response2.json()["user_id"]
 
         assert user_id_from_login_method == user_id_from_auth_method, "User id from auth method is not equal user id from login method"
+
+
+    exclude_params = ["no_cookie", "no_token"]
+    @pytest.mark.parametrize("condition", exclude_params)
+    def test_negative_auth_user(self, condition):
+
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'}
+
+        response1 = requests.post('https://playground.learnqa.ru/api/user/login', data=data)
+        assert "auth_sid" in response1.cookies, "There is no auth_sid cookie in response"
+        assert "x-csrf-token" in response1.headers, "There is no x-csrf-token header in response"
+        assert "user_id" in response1.json(), "There is no user_id in response"
+
+        auth_sid = response1.cookies.get("auth_sid")
+        token = response1.headers.get("x-csrf-token")
+
+        if condition == "no_cookie":
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+                                     headers={"x-csrf-token": token})
+        else:
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+                                     cookies={"auth_sid": auth_sid})
+
+        assert "user_id" in response2.json(), "There is no user_id in second response"
+
+        user_id_from_auth_method = response2.json()["user_id"]
+        assert user_id_from_auth_method == 0, "User is authorized with data only: {condition}"
