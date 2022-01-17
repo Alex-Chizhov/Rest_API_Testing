9df82c09825bb796bd2dac5f2e4602a6c22ff619
Alex <alexchizhov90@gmail.com>
2022-01-17 16:36:39 +0300

    Added assert_json_has_key and assert_code_status methods

--- Pytest_фреймворк/lib/assertions.py
+++ Pytest_фреймворк/lib/assertions.py
@@ -12,3 +12,17 @@ class Asserions:
 
         assert key_name in response_as_dict, f"Response JSON doesn't have key '{key_name}'"
         assert response_as_dict[key_name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, key_name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is {response.text}"
+
+        assert key_name in response_as_dict, f"Response JSON doesn't have key '{key_name}'"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code: {response.status_code}. Expected status code : {expected_status_code}"
