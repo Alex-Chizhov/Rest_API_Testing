71c34a64882a0bd9d561cf05862929844c70ed61
Alex <alexchizhov90@gmail.com>
2022-01-19 12:06:49 +0300

    Added TestUserEdit class with test_edit_user method

---
+++ Pytest_фреймворк/tests/test_user_edit.py
@@ -0,0 +1,52 @@
+import requests
+from ..lib.base_case import BaseCase
+from ..lib.assertions import Asserions
+
+class TestUserEdit(BaseCase):
+
+    def test_edit_user(self):
+        # registration
+        registration_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/",
+                                  data=registration_data
+                                  )
+        Asserions.assert_code_status(response1, 200)
+        Asserions.assert_json_has_key(response1, "id")
+
+
+        email = registration_data["email"]
+        password = registration_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # login
+        login_data = {
+            "email": email,
+            "password": password
+        }
+
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # edit
+        new_name = "New name"
+        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid},
+                                 data={"firstName": new_name}
+                                 )
+
+        Asserions.assert_code_status(response3, 200)
+
+        # get_user_info
+        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid},
+                                 )
+
+        Asserions.assert_json_value_by_name(
+            response4,
+            key_name="firstName",
+            expected_value=new_name,
+            error_message="Invalid username after editing"
+        )
